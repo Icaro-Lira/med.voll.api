e00aad2c15d313ee42ef2f88066a4ef6c57f62a9
Icaro Lira <icarolira895@gmail.com>
2023-08-03 08:40:02 -0300

    feat: inital rest api with spring boot

---
+++ src/main/java/med/voll/api/controller/PacienteCotroller.java
@@ -0,0 +1,45 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.DadosAtualizacaoMedico;
+import med.voll.api.medico.DadosListagemMedico;
+import med.voll.api.paciente.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteCotroller {
+
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados){
+        var paciente = repository.getReferenceById(dados.id());
+        paciente.atualizarInformacaoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
